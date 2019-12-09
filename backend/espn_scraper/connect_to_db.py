4599dfbba6f03ed6c0ac33812b0f04c752ef286c
Yotam Levanon <46316863+YotamNHL@users.noreply.github.com>
2019-12-09 23:43:07 +0200

    Changed MongoDB connect method.

--- backend/espn_scraper/connect_to_db.py
+++ backend/espn_scraper/connect_to_db.py
@@ -6,12 +6,13 @@ logging.basicConfig(filename='log_file.log', level=logging.WARNING,
                     format='%(asctime)s: %(levelname)s: %(processName)s :%(message)s:')
 
 # Opens the csv sheet that contains the client key.
-with open('credentials.csv', 'r') as input:
-    next(input)
-    reader = csv.reader(input)
-    for line in reader:
-        mongoClientKey = line[0]
+# with open('credentials.csv', 'r') as input:
+#     next(input)
+#     reader = csv.reader(input)
+#     for line in reader:
+#         mongoClientKey = line[0]
 
+mongoClientKey = "mongodb+srv://TalJG:SteveNash13@cluster0-zmj3y.mongodb.net/test?retryWrites=true&w=majority"
 
 # Connects to the MongoDB.
 def connect_db():
